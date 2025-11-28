be2c6b2c9ed8d60da6aa689809ee2bbf1a748679
ian <dldks1290@gmail.com>
2025-11-28 17:30:27 +0900

    department 엔티티 완성

---
+++ src/main/java/com/codeit/hrbank/domain/department/entity/Department.java
@@ -0,0 +1,43 @@
+package com.codeit.hrbank.domain.department.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @Column(name = "department_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name = "established_date",nullable = false)
+    private LocalDateTime establishedDate;
+
+    @CreatedDate
+    @Column(name = "created_at",nullable = false)
+    private LocalDateTime createdAt;
+
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
