8ca69773cb7c6b934fe96c40c32d93d7f5c144d6
OfficialHwempire <genius5375@gmail.com>
2025-11-28 17:44:00 +0900

    first employee

---
+++ src/main/java/com/codeit/hrbank/domain/employee/entity/Employee.java
@@ -0,0 +1,51 @@
+package com.codeit.hrbank.domain.employee.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.Instant;
+
+@Table(name = "employees")
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @Column(name = "position",nullable = false)
+    private String position;
+
+    @CreatedDate
+    @Column(name ="hire_date",nullable = false)
+    private Instant hireDate;
+
+
+    @Column(name = "employee_number",nullable = false,unique = true)
+    private String employeeNumber;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status",nullable = false)
+    private EmployeeStatus status;
+
+    @CreatedDate
+    @Column(name = "created_at",nullable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+}
---
+++ src/main/java/com/codeit/hrbank/domain/employee/entity/EmployeeStatus.java
@@ -0,0 +1,5 @@
+package com.codeit.hrbank.domain.employee.entity;
+
+public enum EmployeeStatus {
+    ACTIVE, ON_LEAVE, RESIGNED
+}
