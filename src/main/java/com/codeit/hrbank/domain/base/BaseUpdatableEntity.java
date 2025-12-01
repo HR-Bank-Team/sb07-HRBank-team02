d035fa069c80b6a8d8051f818eedd12ec64247f8
taeeonkim <myhom43@gmail.com>
2025-12-01 10:07:28 +0900

    feat: BaseEntity, BaseUpdatableEntity 추가

---
+++ src/main/java/com/codeit/hrbank/domain/base/BaseUpdatableEntity.java
@@ -0,0 +1,18 @@
+package com.codeit.hrbank.domain.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public class BaseUpdatableEntity extends BaseEntity{
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
