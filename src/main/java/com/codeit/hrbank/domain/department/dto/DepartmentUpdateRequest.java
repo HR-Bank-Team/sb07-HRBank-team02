0dcf81402f483622210a51785e55fe27baf01857
taeeonkim <myhom43@gmail.com>
2025-12-01 22:02:57 +0900

    fix: DepartmentUpdateRequest 필드 타입 수정 (String → LocalDate)

--- src/main/java/com/codeit/hrbank/domain/department/dto/DepartmentUpdateRequest.java
+++ src/main/java/com/codeit/hrbank/domain/department/dto/DepartmentUpdateRequest.java
@@ -1,8 +1,10 @@
 package com.codeit.hrbank.domain.department.dto;
 
+import java.time.LocalDate;
+
 public record DepartmentUpdateRequest(
         String name,
         String description,
-        String establishedDate
+        LocalDate establishedDate
 ) {
 }
