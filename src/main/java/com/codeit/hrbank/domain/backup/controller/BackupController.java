ce10b691aa01eff7eb428dd7c1833ddc0b6986b2
OfficialHwempire <genius5375@gmail.com>
2025-12-02 08:40:15 +0900

    feat: BackupRepository jpql 기반 메서드 추가, backupcontroller 리퀘스트 추가 구현, 백업 엔티티에 백업 서비스용 생성자 추가

--- src/main/java/com/codeit/hrbank/domain/backup/controller/BackupController.java
+++ src/main/java/com/codeit/hrbank/domain/backup/controller/BackupController.java
@@ -3,14 +3,18 @@ package com.codeit.hrbank.domain.backup.controller;
 import com.codeit.hrbank.domain.backup.dto.request.CursorBackupRequestDto;
 import com.codeit.hrbank.domain.backup.dto.response.BackupDto;
 import com.codeit.hrbank.domain.backup.dto.response.CursorPageResponseBackupDto;
+import com.codeit.hrbank.domain.backup.entity.BackupStatus;
 import com.codeit.hrbank.domain.backup.sevice.BackupService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.engine.jdbc.Size;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 
 @RestController
@@ -21,12 +25,24 @@ public class BackupController {
     private final BackupService backupService;
 
     @GetMapping("")
-    public ResponseEntity<CursorPageResponseBackupDto> getBackupPage(@Valid @RequestBody CursorBackupRequestDto cursorBackupRequestDto){
-        return null;
+    public ResponseEntity<CursorPageResponseBackupDto> getBackupPage(@RequestParam(required = false) String worker
+    , @RequestParam(required = false)BackupStatus status, @RequestParam(required = false) LocalDateTime startedAtFrom,
+                                                                     @RequestParam(required = false) LocalDateTime startedAtTo,
+                                                                     @RequestParam(required = false) String sortDirection,
+                                                                     @RequestParam(required = false) String sortField,
+                                                                     @RequestParam(required = false) Long size
+    ){
+        CursorBackupRequestDto dto = new CursorBackupRequestDto(
+                worker, status, startedAtFrom,startedAtTo,sortDirection, sortField , size
+        );
+
+
+        CursorPageResponseBackupDto backupList = backupService.getBackupList(dto);
+        return new ResponseEntity<>(backupList, HttpStatus.OK);
     }
 
     @PostMapping("")
-    public ResponseEntity<BackupDto> createBackup(HttpServletRequest request){
+    public ResponseEntity<BackupDto> createBackup(HttpServletRequest request) throws Exception {
         BackupDto backup = backupService.createBackup(request);
         return new ResponseEntity<>(backup, HttpStatus.CREATED);
     }
