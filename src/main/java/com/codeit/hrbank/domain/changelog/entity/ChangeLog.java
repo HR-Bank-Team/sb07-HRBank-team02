36924e2dc44cc770b94f6a9966783118b4c82bb0
taeeonkim <myhom43@gmail.com>
2025-12-01 10:17:26 +0900

    refactor: 공용엔티티 상속으로 변경

--- src/main/java/com/codeit/hrbank/domain/changelog/entity/ChangeLog.java
+++ src/main/java/com/codeit/hrbank/domain/changelog/entity/ChangeLog.java
@@ -1,33 +1,24 @@
 package com.codeit.hrbank.domain.changelog.entity;
 
+import com.codeit.hrbank.domain.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "change_logs")
-@EntityListeners(AuditingEntityListener.class)
 @AllArgsConstructor
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ChangeLog {
-  @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE)
-  @Column(name = "change_log_id")
-  private Long id;
+public class ChangeLog extends BaseEntity {
 
   @Column(nullable = false, length = 100)
   @Enumerated(EnumType.STRING)
@@ -36,15 +27,11 @@ public class ChangeLog {
   @Column(name = "ip_address", nullable = false)
   private String ipAddress;
 
-  @Column(name = "at", nullable = false)
+  @Column(nullable = false)
   private LocalDateTime at;
 
   @Column(nullable = false, columnDefinition = "TEXT")
   private String memo;
-
-  @CreatedDate
-  @Column(name = "created_at", nullable = false)
-  private LocalDateTime createdAt;
 
   @Column(name = "employee_number", nullable = false)
   private String employeeNumber;
