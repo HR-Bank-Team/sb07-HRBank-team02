c5f843612055ef14f1b2aafcc6a842cf59de3ef3
TaeHUN <tehon79@gmail.com>
2025-12-01 15:31:53 +0900

    feat: EmployeeController 최초 구현

---
+++ src/main/java/com/codeit/hrbank/domain/employee/controller/EmployeeController.java
@@ -0,0 +1,66 @@
+package com.codeit.hrbank.domain.employee.controller;
+
+import com.codeit.hrbank.domain.employee.dto.EmployeeCreateRequest;
+import com.codeit.hrbank.domain.employee.dto.EmployeeResponse;
+import com.codeit.hrbank.domain.employee.dto.EmployeeUpdateRequest;
+import com.codeit.hrbank.domain.employee.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    // 직원 전체 조회
+    @GetMapping
+    public ResponseEntity<List<EmployeeResponse>> getAllEmployee() {
+        List<EmployeeResponse> responses = employeeService.getAllEmployee();
+        return ResponseEntity.ok(responses); // 200 OK
+    }
+
+    // 직원 단건 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeResponse> getEmployee(@PathVariable Long id) {
+        EmployeeResponse response = employeeService.getEmployee(id);
+        return ResponseEntity.ok(response); // 200 OK
+    }
+
+    // 직원 등록
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<EmployeeResponse> createEmployee(
+            @RequestPart("employee") EmployeeCreateRequest request,
+            @RequestPart(value = "file", required = false) MultipartFile file
+    ) {
+        EmployeeResponse response = employeeService.createEmployee(request, file);
+        return ResponseEntity
+                .status(HttpStatus.CREATED) // 201 Created
+                .body(response);
+    }
+
+    // 직원 정보 수정
+    @PatchMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<EmployeeResponse> updateEmployee(
+            @PathVariable Long id,
+            @RequestPart("employee") EmployeeUpdateRequest request,
+            @RequestPart(value = "file", required = false) MultipartFile file
+    ) {
+        EmployeeResponse response = employeeService.updateEmployee(id, request, file);
+        return ResponseEntity.ok(response); // 200 OK
+    }
+
+    // 직원 정보 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.noContent().build(); // 204 No Content
+    }
+}
