5d470c0927aaa0e8acd9dbdc8839185ee39079b2
ChoiJiHye950 <principle950@daum.net>
2025-12-01 23:29:22 +0900

    feat: CursorPageRequestDepartmentDto 커스텀 생성자 추가 및 필드 기본값 설정
    - 일부 필드에 대해 기본값 적용 로직 추가 (sortDirection, sortField, size)

--- src/main/java/com/codeit/hrbank/domain/department/dto/CursorPageRequestDepartmentDto.java
+++ src/main/java/com/codeit/hrbank/domain/department/dto/CursorPageRequestDepartmentDto.java
@@ -8,7 +8,22 @@ public record CursorPageRequestDepartmentDto(
     Long idAfter,              // 마지막 id (기존 lastId)
     Direction sortDirection, //desc or asc
     String sortField,          // 정렬 필드 name or establishedDate
-    int size
+    Integer size
 ) {
-
+  // 커스텀 생성자: 일부 필드 기본값 적용
+  public CursorPageRequestDepartmentDto(
+      String nameOrDescription,
+      String cursor,
+      Long idAfter,
+      Direction sortDirection,
+      String sortField,
+      Integer size
+  ) {
+    this.nameOrDescription = nameOrDescription;
+    this.cursor = cursor;
+    this.idAfter = idAfter;
+    this.sortDirection = sortDirection == null ? Direction.ASC : sortDirection; // 기본 DESC
+    this.sortField = sortField == null ? "establishedDate" : sortField;         // 기본 establishedDate
+    this.size = size == null ? 10 : size;                                        // 기본 20
+  }
 }
