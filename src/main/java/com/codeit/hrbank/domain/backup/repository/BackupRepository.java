a7bc13768f04e9251d9a9890645cc76f7a273e33
OfficialHwempire <genius5375@gmail.com>
2025-12-02 08:40:15 +0900

    feat: BackupRepository jpql 기반 메서드 추가, backupcontroller 리퀘스트 추가 구현, 백업 엔티티에 백업 서비스용 생성자 추가

--- src/main/java/com/codeit/hrbank/domain/backup/repository/BackupRepository.java
+++ src/main/java/com/codeit/hrbank/domain/backup/repository/BackupRepository.java
@@ -1,10 +1,40 @@
 package com.codeit.hrbank.domain.backup.repository;
 
 import com.codeit.hrbank.domain.backup.entity.Backup;
+import com.codeit.hrbank.domain.backup.entity.BackupStatus;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 public interface BackupRepository extends JpaRepository<Backup, Long> {
 
+    @Query("select bu from Backup bu where bu.endedAt = (select  max(su.endedAt) from Backup su)")
+    Backup getLatestBackup();
+
+    @Query("select bu from Backup bu where bu.worker Like %?1% ")
+    List<Backup> getBackupsWithWorker(String worker);
+
+    @Query("select bu from Backup bu where bu.status = ?1")
+    List<Backup> getBackupsByStatus(BackupStatus status);
+
+    @Query("select bu from Backup bu where bu.createdAt between ?1 and ?2")
+    List<Backup> getBackupByTime(LocalDateTime startedAtFrom,LocalDateTime startedAtTo);
+
+    @Query("""
+    select bu from Backup bu where (?1 is null or bu.worker like %?1%)
+    and (?2 is null or bu.status = ?2)
+    and (?3 is null or bu.endedAt between ?3 and ?4)
+    order by 
+        case when ?5 = 'asc' then bu.endedAt end asc,
+        case when ?5 = 'desc' then bu.endedAt end desc
+    
+""")
+    Slice<Backup> getBackupSlice(String worker, BackupStatus status, LocalDateTime startedAtFrom,
+                                 LocalDateTime startedAtTo,String sortDirection,
+                                 String sortField, Long size);
 }
