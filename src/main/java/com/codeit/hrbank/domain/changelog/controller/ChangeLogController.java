7dbd9e13f66356d7f67a137e0ded295adc6484a1
ian <dldks1290@gmail.com>
2025-12-02 15:34:20 +0900

    feature: 수정 로그 검색 쿼리 메서드 구현, 커스텀 리포지토리 인터페이스 만든 후 구현체에 수동 동적 쿼리 작성

--- src/main/java/com/codeit/hrbank/domain/changelog/controller/ChangeLogController.java
+++ src/main/java/com/codeit/hrbank/domain/changelog/controller/ChangeLogController.java
@@ -2,14 +2,13 @@ package com.codeit.hrbank.domain.changelog.controller;
 
 import com.codeit.hrbank.domain.changelog.dto.ChangeLogFilter;
 import com.codeit.hrbank.domain.changelog.dto.CursorPageResponseChangeLogDto;
-import com.codeit.hrbank.domain.changelog.sevice.ChangeLogService;
+import com.codeit.hrbank.domain.changelog.dto.DiffDto;
+import com.codeit.hrbank.domain.changelog.service.ChangeLogService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/change-logs")
@@ -21,8 +20,23 @@ public class ChangeLogController {
     @GetMapping
     public ResponseEntity<CursorPageResponseChangeLogDto> getChangeLogs(
             @ModelAttribute ChangeLogFilter changeLogFilter
-            ){
-
+    ) {
+        changeLogService.getChangeLogs(changeLogFilter);
         return null;
+    }
+
+    @GetMapping("/{id}/diffs")
+    public ResponseEntity<List<DiffDto>> getChangeLogDetails(
+            @PathVariable Long id){
+        return null;
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Integer> getLogCount(
+            @RequestParam String fromDate,
+            @RequestParam String toDate
+    ){
+        return null;
+
     }
 }
---
+++ src/main/java/com/codeit/hrbank/domain/changelog/service/ChangeLogService.java
@@ -0,0 +1,145 @@
+package com.codeit.hrbank.domain.changelog.service;
+
+import com.codeit.hrbank.domain.changelog.dto.ChangeLogDto;
+import com.codeit.hrbank.domain.changelog.dto.ChangeLogFilter;
+import com.codeit.hrbank.domain.changelog.dto.CursorPageResponseChangeLogDto;
+import com.codeit.hrbank.domain.changelog.dto.DiffDto;
+import com.codeit.hrbank.domain.changelog.entity.ChangeLog;
+import com.codeit.hrbank.domain.changelog.entity.ChangeLogType;
+import com.codeit.hrbank.domain.changelog.entity.Diff;
+import com.codeit.hrbank.domain.changelog.mapper.ChangeLogMapper;
+import com.codeit.hrbank.domain.changelog.repository.ChangeLogRepository;
+import com.codeit.hrbank.domain.changelog.repository.DiffRepository;
+import com.codeit.hrbank.domain.employee.entity.Employee;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class ChangeLogService {
+
+    private final ChangeLogRepository changeLogRepository;
+    private final DiffRepository diffRepository;
+
+    @Transactional
+    public void recordLogByAddEmployee(
+            String employeeNumber,
+            String ipAddress,
+            String memo,
+            Employee employee) {
+        ChangeLog changeLog = new ChangeLog(ChangeLogType.CREATED, ipAddress, LocalDateTime.now(), memo, employeeNumber);
+        ChangeLog savedLog = changeLogRepository.save(changeLog);
+
+        List<Diff> diffs = extractDetailsByAdd(employee, savedLog);
+        diffRepository.saveAll(diffs);
+
+    }
+
+    @Transactional
+    public void recordLogByUpdateEmployee(
+            String employeeNumber,
+            String ipAddress,
+            String memo,
+            List<DiffDto> diffDtos) {
+        ChangeLog changeLog =
+                new ChangeLog(ChangeLogType.UPDATED, ipAddress, LocalDateTime.now(), memo, employeeNumber);
+        ChangeLog savedLog = changeLogRepository.save(changeLog);
+
+        List<Diff> diffs = new ArrayList<>();
+        for (DiffDto diffDto : diffDtos) {
+            diffs.add(new Diff(diffDto.toString(), diffDto.getBefore(), diffDto.getAfter(), savedLog));
+        }
+        diffRepository.saveAll(diffs);
+    }
+
+    @Transactional
+    public void recordLogByDeleteEmployee(
+            String employeeNumber,
+            String ipAddress,
+            Employee employee) {
+        ChangeLog changeLog = new ChangeLog(ChangeLogType.DELETED, ipAddress, LocalDateTime.now(), ChangeLogType.DELETED.getValue(), employeeNumber);
+        ChangeLog savedLog = changeLogRepository.save(changeLog);
+
+        List<Diff> diffs = extractDetailsByDelete(employee, savedLog);
+        diffRepository.saveAll(diffs);
+    }
+
+    @Transactional
+    public CursorPageResponseChangeLogDto getChangeLogs(ChangeLogFilter request) {
+        List<ChangeLog> changeLogs = changeLogRepository.searchChangeLogs(
+                request.getEmployeeNumber(),
+                request.getMemo(),
+                request.getIpAddress(),
+                request.getAtFrom(),
+                request.getAtTo(),
+                request.getType(),
+                request.getCursor(),
+                request.getSortField(),
+                request.getSortDirection(),
+                request.getIdAfter(),
+                request.getSize()+1);
+
+        System.out.println(changeLogs.size());
+        boolean hasNext = changeLogs.size() > request.getSize();
+        int endIndex = Math.min(changeLogs.size(), request.getSize());
+        List<ChangeLog> result = changeLogs.subList(0, endIndex);
+        ChangeLog last = result.isEmpty() ? null : result.get(result.size() - 1);
+
+        String nextCursor = null;
+        Long nextIdAfter = null;
+
+        if (request.getSortField().equals("at") && last != null) {
+            nextCursor = last.getAt().atZone(ZoneId.systemDefault()).toInstant().toString();
+            nextIdAfter = last.getId();
+        } else if (request.getSortField().equals("ipAddress") && last != null) {
+            nextCursor = last.getIpAddress();
+            nextIdAfter = last.getId();
+        }
+
+        Long totalElements = changeLogRepository.countChangeLogs(
+                request.getEmployeeNumber(),
+                request.getMemo(),
+                request.getIpAddress(),
+                request.getAtFrom(),
+                request.getAtTo(),
+                request.getType());
+
+        List<ChangeLogDto> list = result.stream()
+                .map(ChangeLogMapper::toDto)
+                .toList();
+        return new CursorPageResponseChangeLogDto(list, nextCursor, nextIdAfter, list.size(), totalElements, hasNext);
+
+
+    }
+
+
+    private List<Diff> extractDetailsByAdd(Employee employee, ChangeLog changeLog) {
+        List<Diff> result = new ArrayList<>();
+        result.add(new Diff("입사일", null, employee.getHireDate().toString(), changeLog));
+        result.add(new Diff("이름", null, employee.getName(), changeLog));
+        result.add(new Diff("직함", null, employee.getPosition(), changeLog));
+        result.add(new Diff("부서명", null, employee.getDepartment().getName(), changeLog));
+        result.add(new Diff("이메일", null, employee.getEmail(), changeLog));
+        result.add(new Diff("사번", null, employee.getEmployeeNumber(), changeLog));
+        result.add(new Diff("상태", null, employee.getStatus().toString(), changeLog));
+        return result;
+    }
+
+    private List<Diff> extractDetailsByDelete(Employee employee, ChangeLog changeLog) {
+        List<Diff> result = new ArrayList<>();
+        result.add(new Diff("입사일", null, employee.getHireDate().toString(), changeLog));
+        result.add(new Diff("이름", null, employee.getName(), changeLog));
+        result.add(new Diff("직함", null, employee.getPosition(), changeLog));
+        result.add(new Diff("부서명", null, employee.getDepartment().getName(), changeLog));
+        result.add(new Diff("이메일", null, employee.getEmail(), changeLog));
+        result.add(new Diff("상태", null, employee.getStatus().toString(), changeLog));
+        return result;
+    }
+}
