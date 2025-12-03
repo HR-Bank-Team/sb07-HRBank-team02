9a8af2748ce4394086197da49c16f89db947e4f5
ian <dldks1290@gmail.com>
2025-12-03 11:38:11 +0900

    refactor: @Transcational(readOnly=true) 추가

--- src/main/java/com/codeit/hrbank/domain/changelog/repository/DiffRepository.java
+++ src/main/java/com/codeit/hrbank/domain/changelog/repository/DiffRepository.java
@@ -1,6 +1,5 @@
 package com.codeit.hrbank.domain.changelog.repository;
 
-import com.codeit.hrbank.domain.changelog.entity.ChangeLog;
 import com.codeit.hrbank.domain.changelog.entity.Diff;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +7,4 @@ import java.util.List;
 
 public interface DiffRepository extends JpaRepository<Diff, Long> {
     List<Diff> findByChangeLogId(Long changeLogId);
-
-    List<Diff> findByChangeLog(ChangeLog changeLog);
 }
