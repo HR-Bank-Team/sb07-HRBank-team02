1455e6f79a7e27d1f59e24b00fce69fe5e4965f0
OfficialHwempire <genius5375@gmail.com>
2025-12-04 17:27:01 +0900

    feature: 정적 리소스에 맞게 repository 쿼리 변경 , instant 값 받는 거 한국 지역으로 변환

--- src/main/java/com/codeit/hrbank/domain/backup/repository/BackSliceRepository.java
+++ src/main/java/com/codeit/hrbank/domain/backup/repository/BackSliceRepository.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public class BackupTestRepository {
+public class BackSliceRepository {
 
 
     @PersistenceContext
--- src/main/java/com/codeit/hrbank/domain/backup/sevice/IBackupService.java
+++ src/main/java/com/codeit/hrbank/domain/backup/sevice/IBackupService.java
@@ -8,14 +8,10 @@ import com.codeit.hrbank.domain.backup.entity.BackupStatus;
 import com.codeit.hrbank.domain.backup.mapper.BackupMapper;
 import com.codeit.hrbank.domain.backup.mapper.CursorPageBackupMapper;
 import com.codeit.hrbank.domain.backup.repository.BackupRepository;
-import com.codeit.hrbank.domain.backup.repository.BackupTestRepository;
+import com.codeit.hrbank.domain.backup.repository.BackSliceRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,13 +29,12 @@ public class IBackupService implements BackupService{
     private final BackupMapper backupMapper;
     private final BackupRegister backupRegister;
     private final CursorPageBackupMapper cursorPageBackupMapper;
-    private final BackupTestRepository backupTestRepository;
+    private final BackSliceRepository backSliceRepository;
 
 
     @Override
     @Transactional(readOnly = true)
     public CursorPageResponseBackupDto getBackupList(CursorBackupRequestDto cursorBackupRequestDto) {
-
 
         String worker = cursorBackupRequestDto.worker();
         LocalDateTime start = cursorBackupRequestDto.startedAtFrom()==null
@@ -54,17 +49,7 @@ public class IBackupService implements BackupService{
         String sortDirection = cursorBackupRequestDto.sortDirection();
         int size = cursorBackupRequestDto.size();
 
-        String safeSortField = (sortField == null || sortField.isBlank()) ? "endedAt" : sortField;
-        Sort.Direction direction = "DESC".equalsIgnoreCase(sortDirection) ? Sort.Direction.DESC : Sort.Direction.ASC;
-
-        Sort sort = Sort.by(direction ,safeSortField);
-        Pageable pageable = PageRequest.of( 0,size+1
-        , sort
-        );
-
-
-        Slice<Backup> backupSlice;
-        var backupList =backupTestRepository.getBackupSlice(
+        var backupList = backSliceRepository.getBackupSlice(
                 worker,
                 status,
                 start,
@@ -77,8 +62,7 @@ public class IBackupService implements BackupService{
         );
         List<BackupDto> backupDtoList = backupList.stream().map(backupMapper::toDto).toList();
 
-
-        Long totalElement = backupTestRepository.countBackup(
+        Long totalElement = backSliceRepository.countBackup(
                 worker,
                 status,
                 start,
