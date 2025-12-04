5343806185a019bbb9a7d25b5b0c046099b52768
OfficialHwempire <genius5375@gmail.com>
2025-12-04 11:22:09 +0900

    refactor: @enableScheduler 를 main 이 아닌 config 로 뺐습니다.

--- src/main/java/com/codeit/hrbank/HrBankApplication.java
+++ src/main/java/com/codeit/hrbank/HrBankApplication.java
@@ -2,12 +2,10 @@ package com.codeit.hrbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableScheduling
 public class HrBankApplication {
     public static void main(String[] args) {
         SpringApplication.run(HrBankApplication.class, args);
